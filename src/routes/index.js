955f25f2a0404cd7c1d5c7fa08be4bcfaaab432c
Kenneth Adrian Buenavista <kennethbuenavista.ccs@gmail.com>
2023-09-04 20:40:03 +0800

    Mapped out recipes per page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ export const routes = [
   { id: 1, path: '/', isExact: true, component: <Home/>},
   { id: 2, path: '/create', component: <Create/> },
   { id: 3, path: '/search', component: <Search/> },
-  { id: 4, path: '/recipe/:id', component: <Recipe/> }
+  { id: 4, path: '/recipe/:id', component: <Recipe/> },
+  { id: 5, path: '*', component: <p>Path Not Found</p> }
 ]
