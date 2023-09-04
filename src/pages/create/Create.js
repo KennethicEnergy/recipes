867e2785d0a55d75845311a7bde6b0d9306a3045
Kenneth Adrian Buenavista <kennethbuenavista.ccs@gmail.com>
2023-09-04 22:09:41 +0800

    added posting of new recipe to json-server

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,10 @@
-import { useRef, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 // styles
 import './Create.css';
+//hooks
+import { useFetch } from '../../hooks/index';
 
 export default function Create() {
   const [title, setTitle] = useState('')
@@ -9,10 +13,14 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
+  const navigate = useNavigate ()
+
+  const { postData, data } = useFetch('http://localhost:3000/recipes', 'POST')
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(title, method, cookingTime, ingredients)
+    postData({ title, ingredients, method, cookingTime: `${cookingTime} minutes` })
   }
 
   const handleAdd = (e) => {
@@ -24,6 +32,13 @@ export default function Create() {
     setNewIngredient('')
     ingredientInput.current.focus();
   }
+
+
+  useEffect(() => {
+    if (data) {
+      navigate('/')
+    }
+  }, [data])
 
   return (
     <div className="create">
